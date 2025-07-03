1957175562e9dad97db7ae64f559d72f8afe6490
shubhada-kulkarni <s.kulkarni@dkfz-heidelberg.de>
2025-07-03 18:07:31 +0200

    Changed whole script to incorporate Seurat UMAP embedding into monocle trajectory - this works

--- monocle_trajectory.R
+++ monocle_trajectory.R
@@ -1,20 +1,32 @@
 library(monocle3)
+library(RColorBrewer)
+library(Seurat)
 
-gene_annotation = as.data.frame(rownames(xenium.obj.female_mi_niche@assays$SCT@counts))
-colnames(gene_annotation) = "gene_short_name"
-rownames(gene_annotation) = gene_annotation$gene_short_name
-
-
-cds <- new_cell_data_set(
-  xenium.obj.female_mi_niche@assays$SCT@counts,
-  cell_metadata = xenium.obj.female_mi_niche@meta.data,
-  gene_metadata = gene_annotation
-)
-
-cds <- preprocess_cds(cds, num_dim = 30)
-cds <- reduce_dimension(cds)
+xenium.obj.female_mi$celltype <- Idents(xenium.obj.female_mi)
+data <- as(as.matrix(xenium.obj.female_mi@assays$SCT@counts), 'sparseMatrix')
+cell_metadata <- data.frame(xenium.obj.female_mi@meta.data)
+gene_annotation <- data.frame(rownames(data))
+colnames(gene_annotation) <- "gene_short_name"
+rownames(gene_annotation) <- gene_annotation$gene_short_name
+cds <- new_cell_data_set(data,cell_metadata  = cell_metadata,gene_metadata  = gene_annotation)
+cds <- preprocess_cds(cds, num_dim = 100)
+cds <- reduce_dimension(cds,preprocess_method = 'PCA',reduction_method = c("UMAP"),cores = 0)
+cds@int_colData@listData$reducedDims$UMAP <- xenium.obj.female_mi@reductions$umap@cell.embeddings
+cds@clusters$UMAP$clusters <- xenium.obj.female_mi@active.ident
 cds <- cluster_cells(cds)
+
+#color by seurat cluster
+plot_cells(cds, label_groups_by_cluster = FALSE, color_cells_by = "celltype")
+
+#color by monocle cluster
+plot_cells(cds, color_cells_by = "partition", label_groups_by_cluster=FALSE)
 cds <- learn_graph(cds)
-plot_cells(cds, color_cells_by="celltype", trajectory_graph_color = "black", group_label_size = 5,
-           graph_label_size = 5)
-cds <- order_cells(cds) # this step requires interactive selection for ordering the cells.
+plot_cells(cds, color_cells_by = "celltype", label_groups_by_cluster=FALSE, 
+           label_leaves=F, label_branch_points=F, cell_size = 1.5)
+cds <- order_cells(cds, reduction_method = "UMAP")
+plot_cells(cds, color_cells_by = "pseudotime", label_groups_by_cluster=FALSE, 
+           label_leaves=F, label_branch_points=F)
+
+# Plotting genes in pseudotime
+plot_genes_in_pseudotime(cds["Ankrd1-FSJ2",], color_cells_by="celltype", 
+                              min_expr=0.5, ncol = 2)
